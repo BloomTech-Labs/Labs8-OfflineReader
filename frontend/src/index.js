f967eb42dbafc394813473be225bc77712bfe8c2
Vera <38669832+VeraSimon@users.noreply.github.com>
2018-11-07 13:40:46 -0800

    Update index.js
    Removed a commented out import that was no longer needed and updated the App import to reflect creation of a `src/components/index.js` with a proper export in it. Verified locally that things still run and test successfully.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { BrowserRouter as Router } from 'react-router-dom';
-import { App } from './components/App';
+import { App } from './components';
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));

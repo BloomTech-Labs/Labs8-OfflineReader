bd34f3757ff4783f50cc5348f982ddd6a6e92e8c
Andrew <ghobrial.andrew@gmail.com>
2018-11-08 15:00:05 -0500

    SignInPage tests complete

--- frontend/src/__tests__/SignInPage.test.js
+++ frontend/src/__tests__/SignInPage.test.js
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SignInPage from '../components/SignInPage/SignInPage';
 
-it('renders without crashing', () => {
-	const div = document.createElement('div');
-	ReactDOM.render(<SignInPage />, div);
-	ReactDOM.unmountComponentAtNode(div);
+describe('<SignInPage />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<SignInPage />, div);
+  });
+   describe('Test default state values', () => {
+    it('this.state.username should be blank by default', () => {
+      const wrapper = shallow(<SignInPage />);
+      const instance = wrapper.instance();
+       expect(instance.state.username).toBe('');
+    });
+     it('this.state.password should be blank by default', () => {
+      const wrapper = shallow(<SignInPage />);
+      const instance = wrapper.instance();
+       expect(instance.state.password).toBe('');
+    });
+  }); 
 });
-

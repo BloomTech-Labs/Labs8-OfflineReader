ba331fc908b1aa6a3a524a2447d9dcf3014c2732
Eva <evalee904@gmail.com>
2018-11-07 18:26:29 -0600

    Basic React sign in page.

--- frontend/src/components/SignInPage/SignInPage.js
+++ frontend/src/components/SignInPage/SignInPage.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function SignInPage() {
-	return <div>Sign In</div>;
+class SignInPage extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: '',
+			password: ''
+		};
+	}
+
+	handleInput = e => {
+		this.setState({ [e.target.name]: e.target.value });
+	};
+
+	handleSubmit = e => {
+		e.preventDefault();
+		const user = this.state.username;
+		localStorage.setItem('user', user);
+		window.location.reload();
+	};
+
+	render() {
+		return (
+			<div>
+				<h2>Sign In</h2>
+				<form className="signin" onSubmit={this.handleSubmit}>
+					<input
+						type="text"
+						className="input"
+						name="username"
+						placeholder="username"
+						value={this.state.username}
+						onChange={this.handleInput}
+					/>
+					<input
+						type="text"
+						className="input"
+						name="password"
+						placeholder="password"
+						value={this.state.password}
+						onChange={this.handleInput}
+					/>
+					<button>Enter</button>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default SignInPage;

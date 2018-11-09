6825dc5b668e4a54fab8988b1ffe312e4c9b5dd3
Andrew <ghobrial.andrew@gmail.com>
2018-11-08 22:13:19 -0500

    fixed localhost int issue and made change to allowed_hosts

--- backend/django/project/settings.py
+++ backend/django/project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-from decouple import config
+from decouple import config, Csv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': config('NAME'),
         'USER': config('USER'),
         'PASSWORD': config('PASSWORD'),
-        'HOST': config('HOST', cast=int),
+        'HOST': config('HOST'),
         'PORT': config('PORT', cast=int),
     }
 }

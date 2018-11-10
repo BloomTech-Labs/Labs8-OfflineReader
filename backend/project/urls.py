e85e6cd547da082453c8a0b0865e527943a92295
Andrew <ghobrial.andrew@gmail.com>
2018-11-09 20:01:22 -0500

    fixed deleted rest framework code

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -27,6 +27,8 @@ router.register(r'users', UserViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
     path('admin/', admin.site.urls),
     path('api/', include('api.urls'))
 ]

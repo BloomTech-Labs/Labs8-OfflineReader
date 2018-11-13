4213e307c0adf25e7b19706ba4136cd7903b521b
Andrew <ghobrial.andrew@gmail.com>
2018-11-13 16:17:28 -0500

    loginUser fxn in actions

--- frontend/src/store/actions/index.js
+++ frontend/src/store/actions/index.js
@@ -31,3 +31,28 @@ export const registerUser = user => {
 			.catch(err => dispatch({ type: ERROR, err }));
 	};
 };
+
+export const loginUser = user => {
+	return dispatch => {
+		dispatch({ type: FETCH_DATA });
+		axios
+			.post('https://anywhere-reader-test.herokuapp.com/api/users', user)
+			.then(response =>
+				dispatch({
+					type: LOGIN_USER,
+					payload: {
+						//the payload you're giving the API to populate the new user
+					}
+				})
+			)
+			.then(
+				// Re-GET all the notes, with the newly added one included
+				axios
+					.get('https://anywhere-reader-test.herokuapp.com/api/users')
+					.then(response =>
+						dispatch({ type: DATA_FETCHED, payload: response.data })
+					)
+			)
+			.catch(err => dispatch({ type: ERROR, err }));
+	};
+};

a44ba4c1ff9525b0c3e253e27fb5acf0145efea0
Vera Simon <vera.simon01@gmail.com>
2018-11-13 18:05:58 -0800

    project/settings.py
    Added STATIC_ROOT setting for staticfiles app

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -139,9 +139,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 
+# What are these? :3
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 SITE_ID = 1

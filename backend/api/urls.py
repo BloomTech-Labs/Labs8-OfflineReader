a3c79ac6314d3b873ecfd2b7ebdc6f2a799f2d16
Eva <evalee904@gmail.com>
2018-11-15 12:04:32 -0600

    Added comma to prevent breaking codes.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
 
-    path('users/', include('payments.urls'))
+    path('users/', include('payments.urls')),
 ]

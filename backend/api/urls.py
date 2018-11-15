56849b5691f39a14d8641a7bd8809a5f4c161381
Eva <evalee904@gmail.com>
2018-11-15 12:02:09 -0600

    Payments urls config.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
+
+    path('users/', include('payments.urls'))
 ]

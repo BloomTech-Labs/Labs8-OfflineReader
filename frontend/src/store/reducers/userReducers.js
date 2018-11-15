5cd6b1724282e0b66f5b1db3d373486969578168
Andrew <ghobrial.andrew@gmail.com>
2018-11-15 00:19:35 -0500

    updated userReducers to connect with auth

--- frontend/src/store/reducers/userReducers.js
+++ frontend/src/store/reducers/userReducers.js
@@ -29,6 +29,30 @@ export default function auth(state = initialState, action) {
 				errors: null
 			};
 
+		case 'REGISTRATION_SUCCESSFUL':
+			localStorage.setItem('token', action.data.token);
+			return {
+				...state,
+				...action.data,
+				isAuthenticated: true,
+				isLoading: false,
+				errors: null
+			};
+
+		case 'AUTHENTICATION_ERROR':
+		case 'LOGIN_FAILED':
+		case 'REGISTRATION_FAILED':
+		case 'LOGOUT_SUCCESSFUL':
+			localStorage.removeItem('token');
+			return {
+				...state,
+				errors: action.data,
+				token: null,
+				user: null,
+				isAuthenticated: false,
+				isLoading: false
+			};
+
 		case 'AUTHENTICATION_ERROR':
 		case 'LOGIN_FAILED':
 		case 'LOGOUT_SUCCESSFUL':

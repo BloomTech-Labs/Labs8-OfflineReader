c02eee9b3eb8a85a96ecb1457cbe19a46f40aadd
ckirkwood94 <ckirkwood94@gmail.com>
2018-11-15 13:39:55 -0500

    remove extra line of code

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('users/', include('users.urls')),
     url(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
-    url(r'^rest-auth/', include('rest_auth.urls')),
-    url(r'^rest-auth/facebook/$', views.FacebookLogin.as_view(), name='fb_login')
+    url(r'^rest-auth/facebook/$', views.FacebookLogin.as_view(), name='fb_login'),
 ]

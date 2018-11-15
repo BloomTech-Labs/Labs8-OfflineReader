05c68efed67dabab599dafe84b5cae2b54183f0b
ckirkwood94 <ckirkwood94@gmail.com>
2018-11-15 12:37:43 -0500

    reconfigure rest-auth urls and added Facebook Login view

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import include, path
+from django.conf.urls import url, include
+
+from . import views
 
 urlpatterns = [
     path('users/', include('users.urls')),
-    path('rest-auth/', include('rest_auth.urls')),
-    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
+    url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/facebook/$', views.FacebookLogin.as_view(), name='fb_login')
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
+from rest_auth.registration.views import SocialLoginView
+
 
 # Create your views here.
+class FacebookLogin(SocialLoginView):
+    adapter_class = FacebookOAuth2Adapter

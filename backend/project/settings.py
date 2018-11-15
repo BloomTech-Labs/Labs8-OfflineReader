715585c63e16b409d9ee108071b7fd9913234e12
Eva <evalee904@gmail.com>
2018-11-14 18:54:03 -0600

    Removed sessions from setting.py
    Eva Li [11:44 AM]
    Eva Li [11:44 AM]
    Co-authored-by: ckirkwood94 <38875505+ckirkwood94@users.noreply.github.com>

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -108,7 +108,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     )
 }
 

071a644a06c507fdbf49e244399fb8552c4578ee
Vera Simon <vera.simon01@gmail.com>
2018-11-20 20:45:59 -0800

    actions
    Split out actions to "article" and "user" files, and did exporting properly from actions/index.js
    Reorganized the imports in src/index.js for clarity

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './components';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 // import { BrowserRouter as Router } from 'react-router-dom';
-import combineReducers from './store/reducers';
 
+import { App } from './components';
+import combineReducers from './store/reducers';
 // import * as serviceWorker from './serviceWorker';
 
 const store = createStore(combineReducers, applyMiddleware(thunk, logger));

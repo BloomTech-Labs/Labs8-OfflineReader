02ba05242d97012fe85c48104661ac3e4c610aee
Samantha Gatt <smfgatt@gmail.com>
2018-11-21 12:02:22 -0500

    Adds Comments to Authentication UI files
    Adds descriptions to the UIView extension functions and the @IBInspectable variables in the GradientIconView

--- ios/Anywhere Reader/Anywhere Reader/View Controller/Authentication/GradientIconView.swift
+++ ios/Anywhere Reader/Anywhere Reader/View Controller/Authentication/GradientIconView.swift
@@ -12,34 +12,55 @@ import UIKit
     
     var gradientLayer: CAGradientLayer?
     
+    /// Used as the view's layer's mask
     @IBInspectable var iconImage: UIImage? {
         didSet {
             self.addMask(mask: iconImage)
         }
     }
     
+    /// The starting color of the gradient
     @IBInspectable var primaryColor: UIColor? {
         didSet {
+            // Removes all sublayers before adding the new gradient
+            // Use a different way to remove the previous gradientLayer from the layer's sublayers if there are other sublayers you would like to keep.
             layer.sublayers?.removeAll()
+            // Sets the gradient layer and assigns it to the view's gradientLayer variable for future reference
+            // If either of the gradient colors are nil, it will default to the view's backgroundColor. If the view's backgroundColor is nil, it will default to clear since the view will be transparent.
             gradientLayer = self.addGradient(primaryColor: primaryColor ?? self.backgroundColor ?? .clear, secondaryColor: secondaryColor ?? self.backgroundColor ?? .clear)
         }
     }
-    
+
+    /// The ending color of the gradient
     @IBInspectable var secondaryColor: UIColor? {
         didSet {
+            // Removes all sublayers before adding the new gradient
+            // Use a different way to remove the previous gradientLayer from the layer's sublayers if there are other sublayers you would like to keep.
             layer.sublayers?.removeAll()
+            // Sets the gradient layer and assigns it to the view's gradientLayer variable for future reference
+            // If either of the gradient colors are nil, it will default to the view's backgroundColor. If the view's backgroundColor is nil, it will default to clear since the view will be transparent.
             gradientLayer = self.addGradient(primaryColor: primaryColor ?? self.backgroundColor ?? .clear, secondaryColor: secondaryColor ?? self.backgroundColor ?? .clear)
         }
     }
     
+    /// The starting point of the gradient
+    ///
+    /// The gradient's direction is determined by the line created by connecting the startPoint and endPoint.
+    /// If gradientLayer is nil, it will have no effect
     @IBInspectable var startPoint: CGPoint = CGPoint(x: 0.5, y: 0.0) {
         didSet {
+            // Sets the startPoint of gradientLayer
             gradientLayer?.startPoint = startPoint
         }
     }
     
+    /// The ending point of the gradient
+    ///
+    /// The gradient's direction is determined by the line created by connecting the startPoint and endPoint.
+    /// If gradientLayer is nil, it will have no effect
     @IBInspectable var endPoint: CGPoint = CGPoint(x: 0.5, y: 1.0) {
         didSet {
+            // Sets the endPoint of gradientLayer
             gradientLayer?.endPoint = endPoint
         }
     }
--- ios/Anywhere Reader/Anywhere Reader/View Controller/Authentication/UIView+GradientAndMask.swift
+++ ios/Anywhere Reader/Anywhere Reader/View Controller/Authentication/UIView+GradientAndMask.swift
@@ -10,6 +10,16 @@ import UIKit
 
 extension UIView {
     
+    /// Appends a CAGradientLayer to the end of the view's layer's sublayers
+    ///
+    /// Only supports two colors
+    ///
+    /// - Parameters:
+    ///   - primaryColor: The starting UIColor of the gradient
+    ///   - secondaryColor: The ending UIColor of the gradient
+    ///
+    /// - Returns: The appended CAGradientLayer as a discardable result
+    @discardableResult
     func addGradient(primaryColor: UIColor = .red, secondaryColor: UIColor = .orange) -> CAGradientLayer {
         let gradient = CAGradientLayer()
         gradient.frame = self.bounds
@@ -18,16 +28,16 @@ extension UIView {
         return gradient
     }
     
+    /// Adds a mask to the view
+    ///
+    /// Creates a CALayer with the same frame as the parent view, and uses the mask argument as its contents. Sets the view's layer's mask to the new CALayer.
+    ///
+    /// - Parameters:
+    ///   - mask: An optional UIImage that will be used as the mask. Image type should support transparency. Otherwise, the entire view will be masked.
     func addMask(mask image: UIImage?) {
         let maskLayer = CALayer()
         maskLayer.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height)
         maskLayer.contents = image?.cgImage
         self.layer.mask = maskLayer
-    }
-    
-    func addMaskAndGradient(imageName: String) -> CAGradientLayer {
-        let gradient = addGradient()
-        addMask(mask: UIImage(named: imageName)!)
-        return gradient
     }
 }

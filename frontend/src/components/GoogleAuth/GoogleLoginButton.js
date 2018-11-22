cf11da08978f98a6d370b340a204700b38131332
Andrew <ghobrial.andrew@gmail.com>
2018-11-21 19:46:04 -0500

    added correct redirect uri in GoogleLoginButton component, separated google_client_id to .env

--- frontend/src/components/GoogleAuth/GoogleLoginButton.js
+++ frontend/src/components/GoogleAuth/GoogleLoginButton.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { GoogleLogin } from 'react-google-login';
 
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+export const google_client_id = GOOGLE_CLIENT_ID;
+
 const GoogleLoginButton = props => {
 	const responseGoogleSuccess = response => {
 		console.log(response);
@@ -17,13 +20,13 @@ const GoogleLoginButton = props => {
 
 	return (
 		<GoogleLogin
-			clientId="254472747355-6umtrkcedqn00tg7ec17l705ftttam0r.apps.googleusercontent.com"
+			clientId={google_client_id}
 			buttonText="Login with Google"
 			onSuccess={responseGoogleSuccess}
 			onFailure={responseGoogleFailure}
 			className="loginBtn loginBtn--google"
 			prompt="select_account"
-			redirectUri="http://localhost:3000/secret/"
+			redirectUri="https://anywhere-reader-test.herokuapp.com/secret/"
 		/>
 	);
 };

478fe7cc6612ed00757ffb479b3aea4ac63f5528
Conner Alegre <cga0@pm.me>
2018-11-24 18:06:52 -0700

    Fix issue with collection view rotation

--- ios/Anywhere Reader/Anywhere Reader/View Controller/Content Collection/ContentCollectionViewController+FlowLayoutDelegate.swift
+++ ios/Anywhere Reader/Anywhere Reader/View Controller/Content Collection/ContentCollectionViewController+FlowLayoutDelegate.swift
@@ -12,7 +12,7 @@ extension ContentCollectionViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        var width = collectionView.frame.width
+        var width = collectionView.safeAreaLayoutGuide.layoutFrame.width
         
         if (self.view.traitCollection.horizontalSizeClass == .regular) {
             width /= 2
@@ -20,6 +20,10 @@ extension ContentCollectionViewController: UICollectionViewDelegateFlowLayout {
         
         let size = CGSize(width: width, height: 300.0)
         return size
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        collectionView?.collectionViewLayout.invalidateLayout();
     }
 }
 

84c0474866f63e5e66f62678c483295de19d1eee
Conner Alegre <cga0@pm.me>
2018-11-24 18:35:24 -0700

    Fixes issue with notch on iPhone XR, iPhone X, etc

--- ios/Anywhere Reader/Anywhere Reader/View Controller/Content Collection/ContentCollectionViewController+FlowLayoutDelegate.swift
+++ ios/Anywhere Reader/Anywhere Reader/View Controller/Content Collection/ContentCollectionViewController+FlowLayoutDelegate.swift
@@ -11,19 +11,13 @@ import UIKit
 extension ContentCollectionViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
         var width = collectionView.safeAreaLayoutGuide.layoutFrame.width
         
         if (self.view.traitCollection.horizontalSizeClass == .regular) {
             width /= 2
         }
         
-        let size = CGSize(width: width, height: 300.0)
-        return size
-    }
-    
-    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        collectionView?.collectionViewLayout.invalidateLayout();
+        return CGSize(width: width, height: 300.0)
     }
 }
 

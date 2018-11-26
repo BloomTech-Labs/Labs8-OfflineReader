c5ef4cc99bd8d81bbbddfde8c6558f4a5f65c89b
Eva <evalee904@gmail.com>
2018-11-26 17:18:14 -0600

    fixed public apiKey placement.

--- frontend/src/components/CheckoutForm/StripeProviderStub.js
+++ frontend/src/components/CheckoutForm/StripeProviderStub.js
@@ -7,32 +7,15 @@ import MyStoreCheckout from './MyStoreCheckout';
 //App loads the stripe script asynchronously in CDM
 
 class StripeProviderStub extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { stripe: '' };
-	}
-	componentDidMount() {
-		if (window.Stripe) {
-			this.setState({
-				stripe: window.Stripe(process.env.REACT_APP_stripe_pub)
-			});
-		} else {
-			document.querySelector('#stripe-js').addEventListener('load', () => {
-				//Create Stripe instance once Stripe.js loads
-				this.setState({
-					stripe: window.Stripe(process.env.REACT_APP_stripe_pub)
-				});
-			});
-		}
-	}
+	// constructor(props) {
+	// 	super(props);
+	// }
 
 	render() {
 		return (
-			this.state.stripe && (
-				<StripeProvider stripe={this.state.stripe}>
-					<MyStoreCheckout />
-				</StripeProvider>
-			)
+			<StripeProvider apiKey="pk_test_2rPuyENL1sfpQAu8wCPX6Fx5">
+				<MyStoreCheckout />
+			</StripeProvider>
 		);
 	}
 }

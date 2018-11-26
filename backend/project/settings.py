1362359535d2fc8d19f50b098bfb8669ecb4a9a7
Vera <38669832+VeraSimon@users.noreply.github.com>
2018-11-26 13:32:58 -0800

    Update settings.py
    Dropped in a TODO to update CORS_ORIGIN stuff for security and finer grained control

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -176,6 +176,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 SITE_ID = 1
 
 # Stripe config
+# TODO: Update to use CORS_ORIGIN_WHITELIST
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_HEADERS = [

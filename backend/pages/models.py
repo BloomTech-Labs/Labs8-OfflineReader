980802d446a633855f77f27de42cc003769d7c14
Eva <evalee904@gmail.com>
2018-11-27 11:26:31 -0600

    Modified pages models.py properties.

--- backend/pages/models.py
+++ backend/pages/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=128, blank=True, default='')
+    # django adds an auto incrementing id field by default - id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=128)
+    normal_url = models.TextField()
+    resolved_url = models.TextField(blank=True, null=True)
+    date_saved = models.DateTimeField(default=timezone.now)
+    excerpt = models.TextField(blank=True, null=True)

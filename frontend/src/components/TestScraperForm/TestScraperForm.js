4e644b4537eb0ffa2e7209ee15187a5869489ecb
Andrew <ghobrial.andrew@gmail.com>
2018-11-27 20:29:49 -0500

    general TestScraperForm.js structure

---
+++ frontend/src/components/TestScraperForm/TestScraperForm.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class TestScraperForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { value: '' };
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(event) {
+		this.setState({ value: event.target.value });
+	}
+
+	handleSubmit(event) {
+		console.log('Url submitted: ' + this.state.value);
+		event.preventDefault();
+	}
+
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<label>
+					URL:
+					<input
+						type="text"
+						value={this.state.value}
+						onChange={this.handleChange}
+					/>
+				</label>
+				<input type="submit" value="Submit" />
+			</form>
+		);
+	}
+}
+
+export default TestScraperForm;

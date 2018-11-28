c13d8d5084224092810f2803cb4571ef1aecced5
Eva <evalee904@gmail.com>
2018-11-27 18:02:52 -0600

    Made new migration. Refactored pages url

--- backend/pages/models.py
+++ backend/pages/models.py
@@ -1,15 +1,16 @@
 from django.db import models
+import datetime
 
 
 class Article(models.Model):
     # django adds an auto incrementing id field by default - id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=128)
-    author = models.CharField(max_length=128)
-    normal_url = models.TextField()
+    title = models.CharField(max_length=128, blank=True, null=True)
+    author = models.CharField(max_length=128, blank=True, null=True)
+    normal_url = models.TextField(blank=True, null=True)
     resolved_url = models.TextField(blank=True, null=True)
-    date_saved = models.DateTimeField(auto_now_add=True)
+    date_saved = models.DateField(default=datetime.date.today)
     date_published = models.DateTimeField(blank=True, null=True)
     excerpt = models.TextField(blank=True, null=True)
     cover_image = models.TextField(blank=True, null=True)
-    tags = models.CharField(max_length=128)
-    text = models.TextField()
+    tags = models.CharField(max_length=128, blank=True, null=True)
+    text = models.TextField(blank=True, null=True)

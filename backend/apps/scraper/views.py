ccb8b20c029e55a5b6e4e98366af2b7f3185d3e1
ckirkwood94 <ckirkwood94@gmail.com>
2018-11-27 22:10:55 -0500

    Started on view to initialize scraper when enpoint is called

--- backend/apps/scraper/views.py
+++ backend/apps/scraper/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.views import APIView
+from .newspaper_scraper import scrape_article
 
-# Create your views here.
+
+class Scrape(APIView):
+
+    def post(self, request, *args, **kwargs):
+        # print(request.data.get('url'))
+        # print(request.data.get(key, default=None))
+        url = request.data.get('url')
+        print(url)
+        scrape_article(request.data.get(str(url)))
+        return HttpResponse()

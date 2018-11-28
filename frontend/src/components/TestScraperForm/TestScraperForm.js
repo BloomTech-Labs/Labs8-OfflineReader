25c3da7467bf4fc04ac51c892856dcd054aa4429
Andrew <ghobrial.andrew@gmail.com>
2018-11-27 21:29:02 -0500

    competed test scraper url form

--- frontend/src/components/TestScraperForm/TestScraperForm.js
+++ frontend/src/components/TestScraperForm/TestScraperForm.js
@@ -1,45 +1,19 @@
-import React, { Component } from 'react';
-import thunk from 'redux-thunk';
-import PropTypes from 'prop-types';
+import React from 'react';
 
-class TestScraperForm extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { value: '' };
-
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
-
-	handleChange(event) {
-		this.setState({ value: event.target.value });
-	}
-
-	handleSubmit(event) {
-		console.log('Url submitted: ' + this.state.value);
-		event.preventDefault();
-	}
-
-	render() {
-		return (
-			<form onSubmit={this.handleSubmit}>
-				<label>
-					URL:
-					<input
-						type="text"
-						value={this.state.value}
-						onChange={this.handleChange}
-					/>
-				</label>
-				<input type="submit" value="Submit" />
-			</form>
-		);
-	}
-}
-
-TestScraperForm.propTypes = {
-	value: PropTypes.string.isRequired,
-	onChange: PropTypes.func.isRequired
+const TestScraperForm = props => {
+	return (
+		<form>
+			Input Article URL here:
+			<input
+				type="text"
+				name="url"
+				placeholder="www.cnn.com"
+				value={props.inputData.url}
+				onChange={props.handleInput}
+			/>
+			<button onClick={props.handleURL}>Save Article</button>
+		</form>
+	);
 };
 
 export default TestScraperForm;

829ab51988937149984d5fb8186d421cbc20a565
Eva <evalee904@gmail.com>
2018-11-28 00:59:10 -0600

    Added comments in models.py

--- backend/pages/models.py
+++ backend/pages/models.py
@@ -5,12 +5,13 @@ import datetime
 
 class Article(models.Model):
     # django adds an auto incrementing id field by default - id = models.AutoField(primary_key=True)
+    #title is the only required field
     title = models.CharField(max_length=500)
     author = models.CharField(max_length=500, default='')
     normal_url = models.URLField(max_length=500, blank=True, null=True, default='')
     resolved_url = models.URLField(blank=True, null=True, default='')
     date_saved = models.TextField(blank=True, null=True, default='')
-    # date_saved = models.DateField(auto_now_add=True, default=datetime.now)
+    # date_saved = models.DateField(auto_now_add=True, default=datetime.now) - in case this needs to be modified from a TextField to a DateField later.
     date_published = models.TextField(blank=True, null=True, default='')
     excerpt = models.TextField(blank=True, null=True, default='')
     cover_image = models.TextField(blank=True, null=True, default='')

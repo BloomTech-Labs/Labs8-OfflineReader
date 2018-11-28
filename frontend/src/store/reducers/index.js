315392d936be912bbceb3cf0b5a18d1786632f20
Andrew <ghobrial.andrew@gmail.com>
2018-11-28 00:58:31 -0500

    reducers index bug fix

--- frontend/src/store/reducers/index.js
+++ frontend/src/store/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import userReducers from './userReducers';
 import articleReducers from './articleReducers';
-import testScraperFormReducers from './testScraperFormReducers';
+import { testScraperFormReducers } from './testScraperFormReducers';
 
 export default combineReducers({
 	userReducers,

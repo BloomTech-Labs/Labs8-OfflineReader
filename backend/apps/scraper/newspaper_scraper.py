66b5c24e497582dc16e62c282d55426544d59552
ckirkwood94 <ckirkwood94@gmail.com>
2018-11-28 00:14:42 -0500

    Remove hardcoded auth header

--- backend/apps/scraper/newspaper_scraper.py
+++ backend/apps/scraper/newspaper_scraper.py
@@ -2,17 +2,13 @@ from newspaper import Article
 import requests
 
 
-def scrape_article(url="https://www.youtube.com/watch?v=SPAmbUZ9UKk", auth="Token 4e252ce5b05cb76e90dca3e450a8b917a14d61bf"):
+def scrape_article(url, auth):
     headers = {"authorization": auth}
     a = Article(url)
-
     a.download()
     a.parse()
-
+# Endpoint needs to be change for dev server
     r = requests.post('http://127.0.0.1:8000/api/users/pages/',
                       data={'title': a.title}, headers=headers)
 
     return
-
-
-# scrape_article()
--- backend/apps/scraper/views.py
+++ backend/apps/scraper/views.py
@@ -5,11 +5,12 @@ from .newspaper_scraper import scrape_article
 
 
 class Scrape(APIView):
-
+    # Invoked when POST at api/scrape endpoint
     def post(self, request, *args, **kwargs):
-        # print(request.data.get('url'))
-        # print(request.data.get(key, default=None))
+        # Pulls url off request
         url = request.data.get('url')
-        print(url)
-        scrape_article(request.data.get(str(url)))
+        # Gets token and sets header
+        auth = "Token " + str(request.auth)
+
+        scrape_article(url, auth)
         return HttpResponse()

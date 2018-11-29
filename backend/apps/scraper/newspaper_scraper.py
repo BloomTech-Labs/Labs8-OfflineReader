f2bb4984f887f276f1d922d8537337c4e42c8826
ckirkwood94 <ckirkwood94@gmail.com>
2018-11-29 01:16:10 -0500

    Dynamically change post request url based on heroku or not

--- backend/apps/scraper/newspaper_scraper.py
+++ backend/apps/scraper/newspaper_scraper.py
@@ -2,7 +2,7 @@ from newspaper import Article
 import requests
 from django.http import HttpResponse, JsonResponse
 from pages.serializers import ArticleSerializer
-from decouple import config
+from project.settings import API_BASE_URL
 
 
 def scrape_article(url, auth):
@@ -10,8 +10,8 @@ def scrape_article(url, auth):
     a = Article(url)
     a.download()
     a.parse()
-# Endpoint needs to be change for dev server
-    r = requests.post('http://127.0.0.1:8000/pages/',
+
+    r = requests.post(API_BASE_URL + 'pages/',
                       json={'title': a.title}, headers=headers)
 
     new_art = r.json()

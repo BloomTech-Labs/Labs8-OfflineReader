aa5802943e22b1b22eedd27c240f07cd4dd12d8a
Andrew <ghobrial.andrew@gmail.com>
2018-11-29 15:06:52 -0500

    v1 of main articles page

--- frontend/src/components/TestScraperForm/PageList.js
+++ frontend/src/components/TestScraperForm/PageList.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Page from './Page';
 
+import styled from 'styled-components';
+
+const PageListContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+
 const PageList = props => {
 	console.log('props are:', props);
 	return (
-		<div>
+		<PageListContainer>
 			<h1>Your Pages:</h1>
-			{props.pages.map(page => {
+			{props.pages.reverse().map(page => {
 				return <Page page={page} key={page.id} />;
 			})}
-		</div>
+		</PageListContainer>
 	);
 };
 

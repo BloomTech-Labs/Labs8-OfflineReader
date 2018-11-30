c8cc7430f0bcb9e4f5f84b30554bc9e57d788071
ckirkwood94 <ckirkwood94@gmail.com>
2018-11-29 19:04:46 -0500

    Remove requirements.txt and remove reverse method off pagelist method

--- frontend/src/components/TestScraperForm/PageList.js
+++ frontend/src/components/TestScraperForm/PageList.js
@@ -13,7 +13,7 @@ const PageList = props => {
 	return (
 		<PageListContainer>
 			<h1>Your Pages:</h1>
-			{props.pages.reverse().map(page => {
+			{props.pages.map(page => {
 				return <Page page={page} key={page.id} />;
 			})}
 		</PageListContainer>

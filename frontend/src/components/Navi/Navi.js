893b9d985b62380272b9545ef1a463e6bd4278b0
Andrew <ghobrial.andrew@gmail.com>
2018-12-03 13:06:44 -0500

    navi responsive

--- frontend/src/components/Navi/Navi.js
+++ frontend/src/components/Navi/Navi.js
@@ -1,23 +1,38 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-// import styled from 'styled-components';
-
+import styled from 'styled-components';
 import { GoogleAuthContainer } from '../../containers';
+
+const NaviContainer = styled.div`
+	width: 100%;
+	margin-bottom: 2rem;
+	box-shadow: 0px 0px 30px -3px rgba(0, 0, 0, 0.75);
+	display: flex;
+	justify-content: center;
+`;
+
+const NaviSubContainer = styled.div`
+	width: 80%;
+	display: flex;
+	justify-content: space-between;
+`;
 
 class Navi extends Component {
 	render() {
 		return (
-			<div>
-				<h3>Anywhere Reader</h3>
-				<NavLink exact to="/">
-					Home
-				</NavLink>
-				<NavLink to="/testScraperFormContainer"> My Pages </NavLink>
-				<NavLink to="/signIn">Sign In </NavLink>
-				<NavLink to="/signUp">Sign Up </NavLink>
-				<NavLink to="/payment">Payment </NavLink>
-				<GoogleAuthContainer />
-			</div>
+			<NaviContainer>
+				<NaviSubContainer>
+					<h3>Anywhere Reader</h3>
+					<NavLink exact to="/">
+						Home
+					</NavLink>
+					<NavLink to="/testScraperFormContainer"> My Pages </NavLink>
+					<NavLink to="/signIn">Sign In </NavLink>
+					<NavLink to="/signUp">Sign Up </NavLink>
+					<NavLink to="/payment">Payment </NavLink>
+					<GoogleAuthContainer />
+				</NaviSubContainer>
+			</NaviContainer>
 		);
 	}
 }

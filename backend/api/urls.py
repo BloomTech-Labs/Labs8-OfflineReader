64eb5bf47a863934a3922335b4059ff4aed60daf
Vera Simon <vera.simon01@gmail.com>
2018-12-03 11:33:10 -0800

    auth backend
    Added dependencies based off o auth0 backend api example code
    Generated an "auth" app for the backend project to house auth code

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,8 +2,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('users/', include('users.urls')),
-    path('rest-auth/', include('rest_auth.urls')),
-    path('rest-auth/registration/', include('rest_auth.registration.urls')),
-    path('users/', include('pages.urls')),
+    path('pages/', include('pages.urls')),
+    path('payments/', include('payments.urls')),
     path('scrape/', include('apps.scraper.urls'))
 ]
--- backend/auth/urls.py
+++ backend/auth/urls.py
@@ -20,16 +20,9 @@ from django.conf.urls import url, include
 from django.contrib.auth.models import User
 # from rest_framework import routers, serializers, viewsets
 
-# Routers provide an easy way of automatically determining the URL conf.
-# router = routers.DefaultRouter()
-# router.register(r'users', UserViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    # url(r'^', include(router.urls)),
     # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('admin/', admin.site.urls),
-    path('auth/', include('auth.urls'))
-    path('api/', include('api.urls')),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    
 ]

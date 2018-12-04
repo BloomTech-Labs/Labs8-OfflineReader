45cd2cb20ef3e2fe0dfd9643f50022e2767b37d9
Samantha Gatt <smfgatt@gmail.com>
2018-12-04 16:58:57 -0500

    Implements FacebookLogin button in authentication view

--- ios/Anywhere Reader/Anywhere Reader/AppDelegate.swift
+++ ios/Anywhere Reader/Anywhere Reader/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+                
         STPPaymentConfiguration.shared().publishableKey = "pk_test_2rPuyENL1sfpQAu8wCPX6Fx5"
         
         AppearanceHelper.setUpTheme()
@@ -30,6 +31,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
         return SDKApplicationDelegate.shared.application(app, open: url, options: options)
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Calls the 'activate' method to log an app event for use in analytics and advertising reporting.
+        AppEventsLogger.activate(application)
     }
 }
 

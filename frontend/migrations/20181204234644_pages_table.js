53a6c02c3cc0b186d622e97e9c501459237a7966
Andrew <ghobrial.andrew@gmail.com>
2018-12-04 23:56:48 -0500

    commented out cover_image blob for now

--- frontend/migrations/20181204234644_pages_table.js
+++ frontend/migrations/20181204234644_pages_table.js
@@ -9,9 +9,9 @@ exports.up = function(knex, Promise) {
 		tbl.string('date_saved', 256);
 		tbl.string('date_published', 256);
 		tbl.string('excerpt', 256);
-		// Need to circle back for this one
-		tbl.bindBlob('cover_image', 256).notNullable();
-		//
+		// // Need to circle back for this one
+		// tbl.bindBlob('cover_image', 256).notNullable();
+		// //
 		tbl.string('tags', 256);
 		tbl.string('text', 256).notNullable();
 		tbl.timestamp('createdAt').defaultTo(knex.fn.now());

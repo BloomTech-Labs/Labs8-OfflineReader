4f39729ad334ebed727c0dcaf37aae0236730017
Vera Simon <vera.simon01@gmail.com>
2018-12-04 17:50:28 -0800

    Auth HOC
    Added some comments for clarity and cleaned up the formatting a little

--- frontend/src/containers/AuthHOC.js
+++ frontend/src/containers/AuthHOC.js
@@ -5,17 +5,23 @@ import { Redirect } from 'react-router-dom';
 const AuthHOC = Protected => {
 	class ProtectedChild extends Component {
 		render() {
+			{
+				/* Check if the user token from the backend has a status http 200, then return the passed component with props if so */
+			}
 			return this.props.authStatus === '200' ? (
 				<Protected {...this.props} />
 			) : (
+				// Otherwise, redirect to the signin page
 				<Redirect to="/signin" />
 			);
 		}
 	}
+
 	const mapStateToProps = state => ({
 		authStatus: state.userReducers.auth.serverToken.status || ''
 	});
 
+	// returns the constructed component or redirect
 	return connect(mapStateToProps)(ProtectedChild);
 };
 

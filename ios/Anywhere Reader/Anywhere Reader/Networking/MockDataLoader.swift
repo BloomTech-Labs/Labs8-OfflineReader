0840940332868ba2624ea9e4d63b9665a1ff351e
Samantha Gatt <smfgatt@gmail.com>
2018-12-06 15:02:16 -0500

    Implements dependency injection
    Adds a NetworkDataLoader protocol and creates a mock data loader. Refactors network request functions in ArticleController to make use of the mock loader.

---
+++ ios/Anywhere Reader/Anywhere Reader/Networking/MockDataLoader.swift
@@ -0,0 +1,72 @@
+//
+//  MockDataLoader.swift
+//  Anywhere Reader
+//
+//  Created by Samantha Gatt on 12/6/18.
+//  Copyright © 2018 Samantha Gatt. All rights reserved.
+//
+
+import Foundation
+
+class MockDataLoader: NetworkDataLoader {
+    
+    // MARK: - Initializer
+    
+    init(data: Data? = nil, error: Error? = nil) {
+        self.data = data
+        self.error = error
+    }
+    
+    
+    // MARK: - Properties
+    
+    let data: Data?
+    let error: Error?
+    private(set) var request: URLRequest? = nil
+    private(set) var url: URL? = nil
+    
+    
+    // MARK: - NetworkDataLoader functions
+    
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        self.url = url
+        DispatchQueue.global().async {
+            completion(self.data, self.error)
+        }
+    }
+    
+    func loadData(with request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+        self.request = request
+        DispatchQueue.global().async {
+            completion(self.data, self.error)
+        }
+    }
+    
+    func uploadData(to url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        self.url = url
+        DispatchQueue.global().async {
+            completion(self.data, self.error)
+        }
+    }
+    
+    func uploadData(with request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+        self.request = request
+        DispatchQueue.global().async {
+            completion(self.data, self.error)
+        }
+    }
+    
+    func deleteData(with url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        self.url = url
+        DispatchQueue.global().async {
+            completion(self.data, self.error)
+        }
+    }
+    
+    func deleteData(with request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+        self.request = request
+        DispatchQueue.global().async {
+            completion(self.data, self.error)
+        }
+    }
+}

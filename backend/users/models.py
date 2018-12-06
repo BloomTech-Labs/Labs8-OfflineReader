19ed4a3c4603eceb89452382c578beedbd9c2fd3
Eva <evalee904@gmail.com>
2018-12-06 01:31:35 -0600

    Added Premium field to users table.
    Co-authored-by: VeraSimon <38669832+VeraSimon@users.noreply.github.com>

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 
 class CustomUser(AbstractUser):
     name = models.CharField(blank=True, max_length=255)
+    premium = models.DateTimeField(default='1970-01-01 00:00:00')
 
     def __str__(self):
         return self.email

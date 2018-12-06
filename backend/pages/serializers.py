8e1b9e692a6d212da6528a1f0f5289c5343d9165
ckirkwood94 <ckirkwood94@gmail.com>
2018-12-06 18:31:25 -0500

    Comment out serializer

--- backend/pages/serializers.py
+++ backend/pages/serializers.py
@@ -15,10 +15,10 @@ class ArticleViewSet(viewsets.ModelViewSet):
     #queryset = Article.objects.all()
 
     # access granted when logged in
-    def get_queryset(self):
-        user = self.request.user
+    # def get_queryset(self):
+    #     user = self.request.user
 
-        if user.is_anonymous:
-            return Article.objects.none()
-        else:
-            return Article.objects.filter(user=user)
+    #     if user.is_anonymous:
+    #         return Article.objects.none()
+    #     else:
+    #         return Article.objects.filter(user=user)

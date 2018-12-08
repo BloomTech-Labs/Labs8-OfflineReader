b5a5511821ec6d63e9145eec13f516d1d4151d19
Samantha Gatt <smfgatt@gmail.com>
2018-12-08 16:20:04 -0500

    Adds documentation to PreferencesViewController functions

--- ios/Anywhere Reader/Anywhere Reader/View Controller/Content Detail/PreferencesViewController.swift
+++ ios/Anywhere Reader/Anywhere Reader/View Controller/Content Detail/PreferencesViewController.swift
@@ -12,12 +12,8 @@ class PreferencesViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        updateButtonsView()
-        preferencesView.layer.cornerRadius = 12.0
-        preferencesView.layer.shadowColor = UIColor.darkGray.cgColor
-        preferencesView.layer.shadowOffset = CGSize(width: 0, height: 2.5)
-        preferencesView.layer.shadowOpacity = 0.25
-        preferencesView.layer.shadowRadius = 5
+        
+        updateViews()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -47,6 +43,13 @@ class PreferencesViewController: UIViewController {
     
     // MARK: - Actions
     
+    /**
+     Dismisses current instance of PreferencesViewController
+     
+     IBAction for a UITapGestureRecognizer that checks to see if the location of the tap was outside the main view. If it was, the current instace of the view controller is dismissed.
+     
+     - Author: Samantha Gatt
+     */
     @IBAction private func dismissFromTap(_ sender: UITapGestureRecognizer) {
         let location = sender.location(in: view)
         // Makes sure the location of tap was outside the bounds of the preferencesView so it can dismiss the VC
@@ -55,52 +58,117 @@ class PreferencesViewController: UIViewController {
         }
     }
     
-    @IBAction func increaseFontSize(_ sender: Any) {
+    /**
+     Increases current font size by one point
+     
+     - Author: Samantha Gatt
+     */
+    @IBAction private func increaseFontSize(_ sender: Any) {
         let oldSize = themeHelper.getBodyFont().pointSize
         // Increases font size by 1 cgpoint. Increment size can be changed later.
         themeHelper.setBodyFont(name: nil, size: oldSize + 1)
     }
     
-    @IBAction func decreaseFontSize(_ sender: Any) {
+    /**
+     Decreases current font size by one point
+     
+     - Author: Samantha Gatt
+     */
+    @IBAction private func decreaseFontSize(_ sender: Any) {
         let oldSize = themeHelper.getBodyFont().pointSize
         // Decreases font size by 1 cgpoint. Increment size can be changed later.
         themeHelper.setBodyFont(name: nil, size: oldSize - 1)
     }
     
-    @IBAction func whiteThemeButtonTapped(_ sender: Any) {
+    /**
+     Sets current theme to white
+     
+     - Author: Conner Alegre
+     */
+    @IBAction private func whiteThemeButtonTapped(_ sender: Any) {
         themeHelper.setWhiteTheme()
         nightThemeSwitch.setOn(false, animated: true)
     }
     
-    @IBAction func tanThemeButtonTapped(_ sender: Any) {
+    /**
+     Sets current theme to tan
+     
+     - Author: Conner Alegre
+     */
+    @IBAction private func tanThemeButtonTapped(_ sender: Any) {
         themeHelper.setTanTheme()
         nightThemeSwitch.setOn(false, animated: true)
     }
     
-    @IBAction func lightGrayThemeButtonTapped(_ sender: Any) {
+    /**
+     Sets current theme to light gray
+     
+     - Author: Conner Alegre
+     */
+    @IBAction private func lightGrayThemeButtonTapped(_ sender: Any) {
         themeHelper.setLightGrayTheme()
         nightThemeSwitch.setOn(false, animated: true)
     }
     
-    @IBAction func darkGrayThemeButtonTapped(_ sender: Any) {
+    /**
+     Sets current theme to dark gray
+     
+     - Author: Conner Alegre
+     */
+    @IBAction private func darkGrayThemeButtonTapped(_ sender: Any) {
         themeHelper.setDarkGrayTheme()
         nightThemeSwitch.setOn(true, animated: true)
     }
     
-    @IBAction func nightModeSwitch(_ sender: Any) {
+    /**
+     Toggles night mode theme
+     
+     - Author: Conner Alegre
+     */
+    @IBAction private func nightModeSwitch(_ sender: Any) {
         themeHelper.toggleNightMode()
     }
     
-    @IBAction func changeFont(_ sender: Any) {
+    @IBAction private func changeFont(_ sender: Any) {
         // Unhides a not yet made font choice view
     }
     
-    @IBAction func brightnessSliderChanged(_ sender: Any) {
+    /**
+     Changes the brightness of the main UIScreen
+     
+     - Author: Conner Alegre
+     */
+    @IBAction private func brightnessSliderChanged(_ sender: Any) {
         UIScreen.main.brightness = CGFloat(brightnessSlider.value)
     }
     
+    
     // MARK: - Private functions
     
+    /// Sets all UI not easily manipulated in Interface Builder
+    private func updateViews() {
+        updateMainView()
+        updateButtonsView()
+    }
+    
+    /**
+     Configures the main view's corner radius and shadow layer
+     
+     - Author: Samantha Gatt
+     */
+    private func updateMainView() {
+        preferencesView.layer.cornerRadius = 12.0
+        preferencesView.layer.shadowColor = UIColor.darkGray.cgColor
+        preferencesView.layer.shadowOffset = CGSize(width: 0, height: 2.5)
+        preferencesView.layer.shadowOpacity = 0.25
+        preferencesView.layer.shadowRadius = 5
+    }
+    
+    /**
+     Configures each theme button's UI
+     
+     - Author: Conner Alegre
+     */
     private func updateButtonsView() {
         let buttons = [whiteThemeButton,
                        tanThemeButton,

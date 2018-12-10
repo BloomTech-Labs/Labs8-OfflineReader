a6f3182440a988a9a0ad8e121ac4dba8470caa02
Vera Simon <vera.simon01@gmail.com>
2018-12-10 11:36:55 -0800

    FB appID bugfix
    Sets the FB appID for the web frontend based on the value of process.env.NODE_ENV

--- frontend/src/components/FBAuth/FBAuth.js
+++ frontend/src/components/FBAuth/FBAuth.js
@@ -8,6 +8,10 @@ const FBClick = styled.span`
 
 class FBAuth extends Component {
 	state = {
+		fbAppId:
+			process.env.NODE_ENV === 'production'
+				? '719662021765780'
+				: '196409721290076',
 		error: ''
 	};
 
@@ -23,7 +27,7 @@ class FBAuth extends Component {
 
 	render() {
 		return (
-			<FacebookProvider appId="196409721290076">
+			<FacebookProvider appId={this.state.fbAppId}>
 				<Login
 					scope="email"
 					onResponse={this.handleResponse}

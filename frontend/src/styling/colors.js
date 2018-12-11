b148a4f60eb9d70fdbac06054eaa856a958d9065
ckirkwood94 <ckirkwood94@gmail.com>
2018-12-11 17:47:04 -0500

    Start adding color palette

---
+++ frontend/src/styling/colors.js
@@ -0,0 +1,10 @@
+const pearlBush = '#E3DCCA';
+const tundora = '#4D4D4D';
+
+export const colors = {
+	white: '#fff',
+	paper: pearlBush,
+	sorrellBrown: '#CAB994',
+	secondaryText: tundora,
+	mineShaft: '#2B2B2B'
+};

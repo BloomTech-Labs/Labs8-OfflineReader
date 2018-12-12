6a7e5919ee01f34a41be658fd9c1526f6a6e4b0e
Vera Simon <vera.simon01@gmail.com>
2018-12-12 10:42:34 -0800

    revoke token
    Created an endpoint wrapper for /auth/social/revoke-token/ called /auth/revoke_token/ that deletes an access token when the user logs out

--- backend/auth/urls.py
+++ backend/auth/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('rest/', include('rest_auth.urls')),
     path('rest/registration/', include('rest_auth.registration.urls')),
     path('social/', include('rest_framework_social_oauth2.urls')),
-    path('convert_token/', views.convert.as_view())
+    path('convert_token/', views.convert.as_view()),
+    path('revoke_token/', views.revoke.as_view())
 ]

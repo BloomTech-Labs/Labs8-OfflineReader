b4884408fa164985c9595d03b8bad21f86abe2b7
Conner Alegre <cga0@pm.me>
2018-12-12 16:48:25 -0700

    Adds ability to save content from Safari

--- ios/Anywhere Reader/Anywhere Reader Share Extension/ShareViewController.swift
+++ ios/Anywhere Reader/Anywhere Reader Share Extension/ShareViewController.swift
@@ -10,17 +10,30 @@ import UIKit
 import Social
 
 class ShareViewController: SLComposeServiceViewController {
-
+    let sharedURL = "sharedURL"
+    
     override func isContentValid() -> Bool {
         // Do validation of contentText and/or NSExtensionContext attachments here
         return true
     }
 
     override func didSelectPost() {
-        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
-    
-        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+        if let item = extensionContext?.inputItems.first as? NSExtensionItem {
+            if let itemProvider = item.attachments?.first {
+                if itemProvider.hasItemConformingToTypeIdentifier("public.url") {
+                    itemProvider.loadItem(forTypeIdentifier: "public.url", options: nil, completionHandler: { (url, error) -> Void in
+                        if let shareURL = url as? NSURL {
+                            if let shareURLAsString = shareURL.absoluteString {
+                                let sharedUserDefaults = UserDefaults(suiteName: "group.com.lambdaschool.AnywhereReader")
+                                sharedUserDefaults?.set(shareURLAsString, forKey: self.sharedURL)
+                                sharedUserDefaults?.synchronize()
+                            }
+                        }
+                        self.extensionContext?.completeRequest(returningItems: [], completionHandler:nil)
+                    })
+                }
+            }
+        }
     }
 
     override func configurationItems() -> [Any]! {

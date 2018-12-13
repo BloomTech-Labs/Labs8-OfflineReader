94ee25da62600898e319d4d8bf53313e6c5ddf11
ckirkwood94 <ckirkwood94@gmail.com>
2018-12-13 18:24:52 -0500

    added cover images for current media (youtube, vimeo, bandcamp)

--- backend/apps/scraper/bandcamp_scraper.py
+++ backend/apps/scraper/bandcamp_scraper.py
@@ -26,7 +26,7 @@ def scrape_bandcamp(url, auth, user_id):
     iframe = create_iframe_tag(source)
 
     r = requests.post(API_BASE_URL + 'api/pages/',
-                      json={'title': title, 'normal_url': url, 'audio': source, 'html': iframe, 'user_id': user_id}, headers=headers)
+                      json={'title': title, 'cover_image': a.top_image, 'normal_url': url, 'audio': source, 'html': iframe, 'user_id': user_id}, headers=headers)
     new_art = r.json()
     return JsonResponse(new_art, status=201)
 

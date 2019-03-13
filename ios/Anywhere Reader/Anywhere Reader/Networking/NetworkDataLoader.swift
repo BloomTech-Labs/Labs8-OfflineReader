392b8ec493a953c608410ed0e720465a31efd35f
Conner Alegre <cga0@pm.me>
2019-03-13 13:25:07 -0600

    Update NetworkDataLoader, removes unused methods and protocols

--- ios/Anywhere Reader/Anywhere Reader/Networking/NetworkDataLoader.swift
+++ ios/Anywhere Reader/Anywhere Reader/Networking/NetworkDataLoader.swift
@@ -10,44 +10,17 @@ import Foundation
 
 protocol NetworkDataLoader {
     func loadData(with request: URLRequest, completion: @escaping( Data?, Error?) -> Void)
-    func loadData(from url: URL, completion: @escaping( Data?, Error?) -> Void)
     func uploadData(with request: URLRequest, completion: @escaping( Data?, Error?) -> Void)
-    func uploadData(to url: URL, completion: @escaping( Data?, Error?) -> Void)
-    func deleteData(with request: URLRequest, completion: @escaping( Data?, Error?) -> Void)
-    func deleteData(with url: URL, completion: @escaping( Data?, Error?) -> Void)
 }
 
 extension URLSession: NetworkDataLoader {
-    
-    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
-        dataTask(with: url) { data, _, error in
-            completion(data, error)
-            }.resume()
-    }
-    
     func loadData(with request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
         dataTask(with: request) { (data, _, error) in
             completion(data, error)
             }.resume()
     }
-    func uploadData(to url: URL, completion: @escaping (Data?, Error?) -> Void) {
-        dataTask(with: url) { data, _, error in
-            completion(data, error)
-            }.resume()
-    }
     
     func uploadData(with request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
-        dataTask(with: request) { (data, _, error) in
-            completion(data, error)
-            }.resume()
-    }
-    func deleteData(with url: URL, completion: @escaping (Data?, Error?) -> Void) {
-        dataTask(with: url) { data, _, error in
-            completion(data, error)
-            }.resume()
-    }
-    
-    func deleteData(with request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
         dataTask(with: request) { (data, _, error) in
             completion(data, error)
             }.resume()
